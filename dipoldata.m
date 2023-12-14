e03f9f1853a0cb4f731a159ef1d50b7caa0644c7
Najmeh Mirian <42173353+nsmirian@users.noreply.github.com>
2023-12-14 16:54:21 +0100

    Update dipoldata.m

--- dipoldata.m
+++ dipoldata.m
@@ -10,3 +10,21 @@ STRENGTH=getfield('FLASH.MAGNETS/MAGNET.ML/D4FL2XTDS/STRENGTH.SP', data)
 kick=getfield('FLASH.MAGNETS/MAGNET.ML/D4FL2XTDS/KICK.SP', data)
 
 current=('FLASH.MAGNETS/MAGNET.ML/D4FL2XTDS/CURRENT.SP', data)
+
+
+% 1/rho=e B/pcolor
+% B*rho=E/e; 
+
+%%
+% Bending Magnet
+%%
+% B*rho[T.m]=10/2.998 E[GeV]
+
+theta=3.584*3.14/180;
+Leff=0.4444;
+rho=Leff/sin(theta)
+
+B=0.634
+E=0.2998*B*rho
+
+kick=0.2998*B/E *Leff
