5f65cc64a156f6c15fdcd9c148adf7319c82be32
Najmeh Mirian <42173353+nsmirian@users.noreply.github.com>
2023-12-18 17:02:02 +0100

    Update datacollection.py

--- datacollection.py
+++ datacollection.py
@@ -7,6 +7,7 @@ import time
 import pydoocs
 import numpy as np
 import matplotlib.pyplot as plt
+import h5py
 
 # preperation of vectors
 nshot=1000
@@ -23,19 +24,16 @@ com12=pydoocs.read(compressionTD12)
 a,b=com12['data'].shape
 com12=np.zeros([a,b,nshot])
 compressionTD21='FLASH.SDIAG/BCM/FL0.DBC2.1/COMPRESSION.normalized.TD'
-com21=pydoocs.read(compressionTD21)
-a,b=com21['data'].shape
+#com21=pydoocs.read(compressionTD21)
+#a,b=com21['data'].shape
 com21=np.zeros([a,b,nshot])
 ##
-compressionTD21='FLASH.SDIAG/BCM/FL0.DBC2.1/COMPRESSION.normalized.TD'
-com21=pydoocs.read(compressionTD21)
-a,b=com21['data'].shape
-com21=np.zeros([a,b,nshot])
+#
 ##
 compressionTD22='FLASH.SDIAG/BCM/FL0.DBC2.2/COMPRESSION.normalized.TD'
-com22=pydoocs.read(compressionTD22)
+#com22=pydoocs.read(compressionTD22)
 
-a,b=com22['data'].shape
+#a,b=com22['data'].shape
 com22=np.zeros([a,b,nshot])
 #plt.plot(S21['data'][1:,0], S21['data'][1:,1], '.')
 #plt.plot(S22['data'][1:,0], S22['data'][1:,1], '.')
@@ -62,20 +60,25 @@ bamuseed5=np.zeros([a,b,nshot])
 
 #### cammper , do not forget
 cammeraT=''
-
+timespam_img=np.zeros([nshot])
+cam_img=np.zeros([1776,2360,nshot])
 
 for n in range(nshot):
     com11[:,:, n]=pydoocs.read(compressionTD11)['data']
     com12[:,:, n]=pydoocs.read(compressionTD12)['data']
-    com21[:,:, n]=pydoocs.read(compressionTD21)['data']
-    com22[:,:, n]=pydoocs.read(compressionTD22)['data']
+    #com21[:,:, n]=pydoocs.read(compressionTD21)['data']
+    #com22[:,:, n]=pydoocs.read(compressionTD22)['data']
     com51[:,:, n]=pydoocs.read(compressionTD51)['data']
     com52[:,:, n]=pydoocs.read(compressionTD52)['data']
 
     bamuBC1[:,:, n]=pydoocs.read(BAMUBC1)['data']
-    bamuseed5[:,:, n]=pydoocs.read(BAMUBCseed5)['data']
-     time.sleep( 0.1 )
-     print(n)
+    bamuseed5[:,:, n]=pydoocs.read(BAMseed5)['data']
+    
+    cam_img[:,:, n]=pydoocs.read(addr_cam)['data']
+    timespam_img[n]=pydoocs.read(addr_cam)['timestamp']
+    
+    time.sleep( 0.1 )
+    print(n)
     ## camera image
 
 hf = h5py.File('data.h5', 'w')
